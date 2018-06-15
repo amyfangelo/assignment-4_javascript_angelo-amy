24713e2df25b2bbcbb1ae651b68485ef0c820a7c
Amy F. Angelo <amyangelo1@me.com>
2018-06-14 20:07:35 -0400

    presidents added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,52 @@
-var icecream = ['matcha', 'chocolate chip cookie dough', 'Imagine Whirrled Peace'];
-var icecream = 'lavendar';
+//array
+
+var iceCream;
+iceCream = ["matcha", "chocolate", "vanilla"];
+console.log(iceCream);
+
+var iceCream = "lavendar" ;
+
+//objects
+
+var presidents1 = {
+  firstName: 'Donald',
+  lastName: 'Trump',
+  termLength: 1,
+  party: 'Republican',
+  yearsOfPresidency: 1,
+};
+
+var presidents2 = {
+  firstName: 'Barack',
+  lastName: 'Obama',
+  termLength: 2,
+  party: 'Democrat',
+  yearsOfPresidency: 8,
+};
+
+var presidents3 = {
+  firstName: 'George W.',
+  lastName: 'Bush',
+  termLength: 2,
+  party: 'Republican',
+  yearsOfPresidency: 8,
+};
+
+var presidents4 = {
+  firstName: 'Bill',
+  lastName: 'Clinton',
+  termLength: 2,
+  party: 'Democratic',
+  yearsOfPresidency: 8,
+};
+
+var presidents5 = {
+  firstName: 'Ronald',
+  lastName: 'Raegan',
+  termLength: 2,
+  party: 'Republican',
+  yearsOfPresidency: 8,
+};
+
+var presidents = ['presidents1', 'presidents2', 'presidents3', 'presidents4', 'presidents5' ];
+console.log(presidents3.firstName + presidents3.);
