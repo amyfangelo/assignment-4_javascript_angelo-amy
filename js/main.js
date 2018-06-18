baccf2e8bd50d76ffcc199a7e45373ac13043211
Amy F. Angelo <amyangelo1@me.com>
2018-06-18 12:34:31 -0400

    functions edited with Caitlyn

--- js/main.js
+++ js/main.js
@@ -1,55 +1,5 @@
-//arrays & string
-
-var iceCream;
-iceCream = ["matcha", "chocolate", "vanilla"];
-
-var iceCream = "lavendar" ;
 
 
-//objects in array
-
-var presidents1 = {
-  firstName: 'Donald',
-  lastName: 'Trump',
-  termLength: 1,
-  party: 'Republican',
-  yearsOfPresidency: 1,
-};
-
-var presidents2 = {
-  firstName: 'Barack',
-  lastName: 'Obama',
-  termLength: 2,
-  party: 'Democrat',
-  yearsOfPresidency: 8,
-};
-
-var presidents3 = {
-  firstName: 'George W.',
-  lastName: 'Bush',
-  termLength: 2,
-  party: 'Republican',
-  yearsOfPresidency: 8,
-};
-
-var presidents4 = {
-  firstName: 'Bill',
-  lastName: 'Clinton',
-  termLength: 2,
-  party: 'Democratic',
-  yearsOfPresidency: 8,
-};
-
-var presidents5 = {
-  firstName: 'Ronald',
-  lastName: 'Raegan',
-  termLength: 2,
-  party: 'Republican',
-  yearsOfPresidency: 8,
-};
-
-var presidents = ['presidents1', 'presidents2', 'presidents3', 'presidents4', 'presidents5' ];
-console.log(presidents3.firstName + presidents3.lastName);
 
 //functions
 
@@ -59,34 +9,35 @@ function howdy( person, message ) {
 
 var message = howdy('Amy Angelo', 'Hi' );
 
+function howdy(){
+  return('Amy Angelo says hi');
+}
+
+console.log(howdy());
+
 //6b
 
- var numba = function (string) {
-    if (string < 7 )
-return('What a short little word!' );
-
-else if  (string > 7 )
-return ('I am sorry, but that is too many to count' )
-
- else if (string === 7)
-return( '7, what a perfect choice!' );
-
+ var numba = function(string) {
+  if (string.length < 7 ) {
+    return('What a short little word!');
+  } else if (string.length > 7 ) {
+    return('I am sorry, but that is too many to count');
+  } else if (string.length === 7) {
+    return('7, what a perfect choice!');
+  }
 }
 
-function numba (string) {
-  return (
-  console.log ('7'),
-  console.log ('seventy'),
-  console.log ('university')
-)
+console.log(numba('seven'));
+console.log(numba ('university'));
+console.log(numba('7'));
 
-}
 
 function inception( display, favMovie ) {
-display(favMovie);
+  display(favMovie);
 }
 
-function movieLogger (movie) {
+function movieLog(movie) {
     console.log (movie + ' '+ 'is a fantastic movie.');
 }
-inception(movieLogger, 'Shrek');
+
+inception(movieLog, 'Shrek');
